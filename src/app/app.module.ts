e6f35f6482968c36444bce3499f5a65763e5d7b4
Lock-Lee <locklee.on@email.com>
2019-04-13 16:58:10 +0700

    comAss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,34 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { NgxGaugeModule } from 'ngx-gauge';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from "@angular/fire";
+import {
+  AngularFireDatabaseModule,
+  AngularFireDatabase
+} from "@angular/fire/database";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDAumgrN9B_JTgifEGkMt2pYPy9XHQhN-M",
+    authDomain: "smartfram-2ce00.firebaseapp.com",
+    databaseURL: "https://smartfram-2ce00.firebaseio.com",
+    projectId: "smartfram-2ce00",
+    storageBucket: "smartfram-2ce00.appspot.com",
+    messagingSenderId: "784946501466"
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    AngularFireModule.initializeApp(firebaseConfig), AngularFireDatabaseModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgxGaugeModule],
   providers: [
-    StatusBar,
+    StatusBar, AngularFireDatabase,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  public dataDHT: any;
+  public Time: any;
+
+  thresholdConfig = {
+    '0': { color: 'green' },
+    '40': { color: 'orange' },
+    '75.5': { color: 'red' }
+  };
+
+  constructor(
+    public fb: AngularFireDatabase
+  ) {
+    this.fb.list("/List").push({
+      Temperature: Math.random() * 100,
+      Humidity: Math.random() * 10,
+      Time: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} ${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`
+
+    })
+
+    this.fb
+      .object("/Humidity")
+      .valueChanges()
+      .subscribe((value: any) => {
+        console.log(value);
+        this.dataDHT = value;
+      });
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
+
+}
